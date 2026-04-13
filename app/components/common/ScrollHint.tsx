92b27e34c8bf26e5438fbe098a332e394fe3d364
Ashutosh Joshi <ashutxsh.jxshi@gmail.com>
2026-04-13 16:28:46 -0400

    3d view

--- app/components/common/ScrollHint.tsx
+++ app/components/common/ScrollHint.tsx
@@ -51,7 +51,6 @@ export const ScrollHint = () => {
   return (
     <div className="fixed w-full bottom-5 scroll-hint" style={{ opacity: 0 }}>
       <div className="flex items-center justify-center animate-pulse">
-        { showScrollHint }
         <Image src={svgSrc} width={18} height={18} alt="night mode" loading="lazy" />
         <span className="text-white">{hintText}</span>
       </div>
